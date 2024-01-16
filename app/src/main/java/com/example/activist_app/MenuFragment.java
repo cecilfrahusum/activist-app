642a10e41dccac1e3a67cd972e0d498a8d503176
cema <cema@itu.dk>
2024-01-16 13:07:33 +0100

    Add another Fragment and a test button for navigation.

--- app/src/main/java/com/example/activist_app/MenuFragment.java
+++ app/src/main/java/com/example/activist_app/MenuFragment.java
@@ -3,15 +3,18 @@ package com.example.activist_app;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MenuFragment extends Fragment {
 
     private TextView testText;
+    private Button toTalkie;
 
       public MenuFragment() {
         // Required empty public constructor
@@ -26,7 +29,17 @@ public class MenuFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
                 View v = inflater.inflate(R.layout.fragment_menu, container, false);
+
                 testText = v.findViewById(R.id.testText);
+                toTalkie = v.findViewById(R.id.toTalkieButton);
+
+                toTalkie.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Navigation.findNavController(view).navigate(R.id.action_menuFragment_to_talkieFragment);
+                    }
+                });
+
                 return v;
     }
 }
