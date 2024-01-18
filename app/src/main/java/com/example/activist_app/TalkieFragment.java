227a51447937f2d74a8b6956d2ef9e8c02931c71
cema <cema@itu.dk>
2024-01-18 15:45:54 +0100

    Work on UI for the walkie talkie fragment

--- app/src/main/java/com/example/activist_app/TalkieFragment.java
+++ app/src/main/java/com/example/activist_app/TalkieFragment.java
@@ -8,10 +8,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class TalkieFragment extends Fragment {
 
-    private Button TalkieButton;
+    private Button recordButton;
+    private Button sendButton;
 
     public TalkieFragment() {
         // Required empty public constructor
@@ -26,7 +28,23 @@ public class TalkieFragment extends Fragment {
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_talkie, container, false);
 
-        TalkieButton = v.findViewById(R.id.TalkieButton);
+        recordButton = v.findViewById(R.id.recordButton);
+        sendButton = v.findViewById(R.id.sendButton);
+
+        recordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendButton.bringToFront();
+            }
+        });
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "Your message was sent to everyone.", Toast.LENGTH_LONG).show();
+                recordButton.bringToFront();
+            }
+        });
 
         return v;
     }
