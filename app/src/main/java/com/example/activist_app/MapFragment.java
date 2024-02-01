6a5152c9fc7dc39cf746d8d13da67ce700dddfed
cema <cema@itu.dk>
2024-02-01 11:44:16 +0100

    add a FusedLocationProviderClient

--- app/src/main/java/com/example/activist_app/MapFragment.java
+++ app/src/main/java/com/example/activist_app/MapFragment.java
@@ -8,31 +8,47 @@ import androidx.fragment.app.Fragment;
 import android.Manifest;
 import android.app.Activity;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class MapFragment extends Fragment {
 
     String googleApiKey = BuildConfig.GOOGLE_API_KEY;
 
     private PermissionsHandler permissionsHandler = new PermissionsHandler();
+    private static final int REQUEST_PERMISSION = 1;
+    private FusedLocationProviderClient locationClient;
 
-    LatLng defaultPos = new LatLng(55.658619, 12.589548);
+
+    LatLng defaultPos = new LatLng(55.658619, 12.589548); // ITU's location
     private final static int DEFAULT_ZOOM = 15;
 
-    private OnMapReadyCallback callback = new OnMapReadyCallback() {
 
+    private OnMapReadyCallback callback = new OnMapReadyCallback() {
         @Override
         public void onMapReady(GoogleMap googleMap) {
-            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(defaultPos, DEFAULT_ZOOM));
+
+            // Handle if user has not given location permission after being asked
+            if (permissionsHandler.hasLocationPermission(getActivity()) == false) {
+                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(defaultPos, DEFAULT_ZOOM));
+                Toast.makeText(getActivity(), "You will not be able to use the features of the map if you do not allow access to location data.", Toast.LENGTH_LONG).show();
+            }
+
+
         }
     };
 
@@ -41,7 +57,16 @@ public class MapFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        View v = inflater.inflate(R.layout.fragment_map, container, false);
+
+        locationClient = LocationServices.getFusedLocationProviderClient(getActivity()); // is this line actually necessary?
+
+        permissionsHandler.requestLocationPermission(getActivity(), REQUEST_PERMISSION);
+        if (permissionsHandler.hasLocationPermission(getActivity()) == false) {
+            Toast.makeText(getActivity(), "You will not be able to use the features of the map if you do not allow access to location data.", Toast.LENGTH_LONG).show();
+        }
+
+        return v;
     }
 
     @Override
@@ -53,4 +78,5 @@ public class MapFragment extends Fragment {
             mapFragment.getMapAsync(callback);
         }
     }
+
 }
