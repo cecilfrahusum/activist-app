d3cd11019bc61ffce226c24c84972f76098bd5be
cema <cema@itu.dk>
2024-02-13 12:08:15 +0100

    Convert MapFragment to Kotlin

---
+++ app/src/main/java/com/example/activist_app/MapFragment.kt
@@ -0,0 +1,42 @@
+package com.example.activist_app
+
+import androidx.fragment.app.Fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class MapFragment : Fragment() {
+
+    var googleApiKey = BuildConfig.GOOGLE_API_KEY
+    var defaultPos = LatLng(55.658619, 12.589548) // ITU's location
+    var DEFAULT_ZOOM: Float = 15F
+
+
+    private val callback = OnMapReadyCallback { googleMap ->
+
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(defaultPos, DEFAULT_ZOOM))
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_map, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(callback)
+    }
+}
