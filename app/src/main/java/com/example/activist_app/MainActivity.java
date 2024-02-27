d372ddcdf6bdbea829bff7f09ab217eae2e0ae61
cema <cema@itu.dk>
2024-02-27 14:39:58 +0100

    Add top menu (buttons don't do anything yet)

--- app/src/main/java/com/example/activist_app/MainActivity.java
+++ app/src/main/java/com/example/activist_app/MainActivity.java
@@ -13,17 +13,20 @@ import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 
+import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
     NavigationBarView bottomNavigationView;
+    MaterialToolbar topMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        topMenu = findViewById(R.id.top_menu);
         bottomNavigationView = findViewById(R.id.bottom_navigation);
 
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
