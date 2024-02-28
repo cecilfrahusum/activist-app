56da8e46103890c5a7317d49d7608f6dc1650c58
cema <cema@itu.dk>
2024-02-28 12:33:31 +0100

    Add PinDatabase.java

---
+++ app/src/main/java/com/example/activist_app/PinDatabase.java
@@ -0,0 +1,33 @@
+package com.example.activist_app;
+
+import androidx.lifecycle.ViewModel;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PinDatabase extends ViewModel {
+
+    private ArrayList<InfoPin> pins = new ArrayList<>();
+
+    public PinDatabase() {
+        // adding some hardcoded pins
+        LatLng testLocation1 = new LatLng(55.660505, 12.591268);
+        addPin(testLocation1, "testing 1 2.. ");
+    }
+
+    public void addPin(LatLng latLng, String message) {
+        InfoPin infoPin = new InfoPin(latLng, message);
+        pins.add(infoPin);
+    }
+
+    public ArrayList<InfoPin> getPins() {
+        return pins;
+    }
+
+    public int getSize(){
+        return pins.size();
+    }
+
+}
